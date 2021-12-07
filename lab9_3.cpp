64a06f35c16cce89b01fc99584ae8796b07f893d
gunxroswar <gxz.gunforce@gmail.com>
2021-12-07 17:01:21 +0700

    "done lab9"

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,56 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+
+int main()
+{
+    double age;
+    double height;
+    double property;
+
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 20){
+        cout <<"Enter your height: ";
+        cin >> height;
+        if(height < 160){
+            cout << "Your status = UNFRIEND";
+        }else{
+            if(height < 175){
+                cout << "Your status = FRIEND";                
+            }else{
+                cout << "Enter your property: ";
+                cin >> property;
+                if(property > 69000000){
+                    cout << "Your status = MARRIED";
+                }else{
+                    cout << "Your status = ONE-NIGHT-STAND";
+                }
+            }
+        }
+    }else{
+        if(age < 30){
+            cout << "Enter your property: ";
+            cin >> property;
+            if(property > 10000000){
+                cout << "Your status = BEST FRIEND";
+            }else{
+                cout << "Your status = UNFRIEND";
+            }
+
+        }else{
+            cout << "Your status = UNFRIEND";
+        }
+
+    }
+    
+    
+}
+
+
+
 /*
 "Enter your age: "
 "Enter your height: "
